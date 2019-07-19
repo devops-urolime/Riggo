1abe8d20fe2282baa1747c5e2602e7b5c812a9aa
edwardsong <pyNN7KTnwVqtbH>
2019-07-19 15:40:44 -0400

    RIG-322 - Create Services and Repository for LineItem

---
+++ spring-api-services/src/main/java/io/riggo/data/repositories/LoadLineItemRepository.java
@@ -0,0 +1,17 @@
+package io.riggo.data.repositories;
+
+import io.riggo.data.domain.LoadLineItem;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface LoadLineItemRepository extends CrudRepository<LoadLineItem, Long> {
+
+    @Query("select l from LoadLineItem l where l.loadId = :loadId")
+    Optional<List<LoadLineItem>> findByLoadId(@Param("loadId") Integer loadId);
+}
