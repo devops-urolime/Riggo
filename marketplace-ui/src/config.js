75a5cb9e358bc7020573bf31974a98e4d3e59efb
Ismael Terreno <ismael@riggo.io>
2019-07-30 10:38:49 -0300

    RIG-310: - Pie Chart Visualizations - On Time Performance Pickup with support for API format including demo mock functionality. - Added Redux and Sagas support.
    RIG-311:
    - Pie Chart Visualizations - On Time Performance Delivery with support for API format including demo mock functionality.
    - Added Redux and Sagas support.

--- marketplace-ui/src/config.js
+++ marketplace-ui/src/config.js
@@ -1,7 +1,7 @@
 export const BASE_END_POINT='https://o0de6p7v3h.execute-api.us-west-2.amazonaws.com/qa';
 export const EMPTY_JWT_ERROR_MESSAGE='There is no JWT access token to make calls to the APIs';
 export const LOAD_END_POINT='/load';
-export const MENU_END_POINT='/menus';
+export const MENU_END_POINT='/menus/mock';
 export const LOAD_PIPELINE_SUMMARY_END_POINT= LOAD_END_POINT + '/pipeline/summary';
 export const LOAD_STOP_SUMMARY_END_POINT= LOAD_END_POINT + '/stop/summary';
 export const LOAD_READ_SCOPE = "read:load";
