22601747e930fdc0ee0849721a3a4110e9f67358
Ismael Terreno <ismael@riggo.io>
2019-08-05 19:41:45 -0300

    RIG-374:  - Base component for Shipments visualization.

--- marketplace-ui/src/components/MultiYAxesVisualization.js
+++ marketplace-ui/src/components/MultiYAxesVisualization.js
@@ -7,19 +7,19 @@ import './MultiYAxesVisualization.scss';
 
 const data = [
   {
-    date: 'July 1', "Shipment Delivered": 150, "Cost Per Mile": 5.36,
+    date: 'July 1', "Shipment Delivered": 55, "Cost Per Mile": 5.36,
   },
   {
-    date: 'July 2', "Shipment Delivered": 100, "Cost Per Mile": 4.10 ,
+    date: 'July 2', "Shipment Delivered": 40, "Cost Per Mile": 4.10 ,
   },
   {
-    date: 'July 3', "Shipment Delivered": 75, "Cost Per Mile":  3.20,
+    date: 'July 3', "Shipment Delivered": 35, "Cost Per Mile":  3.20,
   },
   {
-    date: 'July 4', "Shipment Delivered": 200, "Cost Per Mile": 8.57 ,
+    date: 'July 4', "Shipment Delivered": 65, "Cost Per Mile": 8.57 ,
   },
   {
-    date: 'July 5', "Shipment Delivered": 125, "Cost Per Mile":  1.7,
+    date: 'July 5', "Shipment Delivered": 15, "Cost Per Mile":  1.7,
   },
   {
     date: 'July 6', "Shipment Delivered": 128, "Cost Per Mile": 3.34 ,
@@ -44,17 +44,17 @@ class MultiYAxesLineAndBar extends PureComponent {
           iconType="square"
           align="center"
         />
-        <Line
-          yAxisId="left"
-          type="monotone"
-          dataKey="Shipment Delivered"
-          stroke="#E84B5E"
-        />
         <Bar
-          yAxisId="right"
-          dataKey="Cost Per Mile"
+          yAxisId="left"
+          dataKey="Shipment Delivered"
           barSize={20}
           fill="#FECEA8"
+        />
+        <Line
+          yAxisId="right"
+          type="monotone"
+          dataKey="Cost Per Mile"
+          stroke="#E84B5E"
         />
       </ComposedChart>
     );
