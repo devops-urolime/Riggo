fe7721e44513fa18fcbf71af81cec7969fe62056
Ismael Terreno <ismael@riggo.io>
2019-08-28 15:25:55 -0300

    RIG-374:  - Mock true shipments visualizations.

--- marketplace-ui/src/api.js
+++ marketplace-ui/src/api.js
@@ -462,5 +462,5 @@ export const loadShipmentSummaryApi = async (offset, units, fiscalMonth, fiscalY
       BASE_END_POINT +
       LOAD_SHIPMENT_SUMMARY_END_POINT +
       `?offset=${offset}&units=${units}&fiscalMonth=${fiscalMonth}&fiscalYear=${fiscalYear}&week=${week}`;
-    return consumeApi(END_POINT, METHOD_GET, JWT, getShipmentsSummaryMock(units), false);
+    return consumeApi(END_POINT, METHOD_GET, JWT, getShipmentsSummaryMock(units), true);
 };
