6cb559beb7db6c4641f5caa752f68acf68b2ce4f
Ismael Terreno <ismael@riggo.io>
2019-09-04 16:30:11 -0300

    RIG-522: Fix for navigation using correct parameter names and mock disabled for shipments viz.

--- marketplace-ui/src/api.js
+++ marketplace-ui/src/api.js
@@ -464,5 +464,5 @@ export const loadShipmentSummaryApi = async (offset, units, fiscalMonth, fiscalY
       BASE_END_POINT +
       LOAD_SHIPMENT_SUMMARY_END_POINT +
       `?offset=${offset}&units=${units}&fiscalMonth=${fiscalMonth}&fiscalYear=${fiscalYear}&fiscalWeek=${fiscalWeek}`;
-    return consumeApi(END_POINT, METHOD_GET, JWT, getShipmentsSummaryMock(units), true);
+    return consumeApi(END_POINT, METHOD_GET, JWT, getShipmentsSummaryMock(units), false);
 };
