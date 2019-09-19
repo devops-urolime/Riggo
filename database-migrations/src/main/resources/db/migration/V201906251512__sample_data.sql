ad320d73e67e8fe778058a0db9a2ebeb35c3cd26
edwardsong <pyNN7KTnwVqtbH>
2019-09-19 17:48:22 -0400

    RIG-567 - Fix DB Scripts

--- database-migrations/src/main/resources/db/migration/V201906251512__sample_data.sql
+++ database-migrations/src/main/resources/db/migration/V201906251512__sample_data.sql
@@ -1,7 +1,7 @@
 /* Sample data updates */
 INSERT INTO public.shipper(
-    id, ext_sys_id, ext_sys_tenant_id, created_at, name, site_id)
-VALUES (1, '', '', now(),  'Hello Shipping', 100);
+    ext_sys_id, ext_sys_tenant_id, created_at, name, site_id)
+VALUES ('', '', now(),  'Hello Shipping', 100);
 
 INSERT INTO public.load(
     ext_sys_id, ext_sys_tenant_id, distance_miles, shipper_id, expected_ship_date, origin, destination, carrier, transport_mode, posted_rate, posted_currency, insurance_amt, insurnace_currency, total_weight, weight_uom, equipment_type, load_status, team_req, food_grade_trailer_req, temp_control_req, expected_delivery_date, driver, dispatcher, location_based_svcs_req, created_at, deleted, reference_number, equipment_type_id)
