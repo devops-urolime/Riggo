cb216561f0262ad6b5d33fb07f0f173feace932f
edwardsong <pyNN7KTnwVqtbH>
2019-09-24 13:13:14 -0400

    RIG-574 - Fix API Issues found during Authoring tests - Set No for unexpected status from SF.

--- spring-api-services/src/main/java/io/riggo/web/integration/resolver/SalesforceRevenovaInvoiceStatusResolver.java
+++ spring-api-services/src/main/java/io/riggo/web/integration/resolver/SalesforceRevenovaInvoiceStatusResolver.java
@@ -1,6 +1,7 @@
 package io.riggo.web.integration.resolver;
 
 import io.riggo.data.domain.InvoiceStatus;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
@@ -11,9 +12,11 @@ public class SalesforceRevenovaInvoiceStatusResolver implements InvoiceStatusRes
 
     public final static String INVOICE_STATUS = "invoice_status";
 
-
     public InvoiceStatus resolveInvoiceStatus(Map<String, Object> parameters){
-        String invoiceStatus = (String) parameters.get(INVOICE_STATUS);
-        return InvoiceStatus.fromDisplayName(invoiceStatus);
+        InvoiceStatus invoiceStatus = null;
+        if(StringUtils.isNotBlank((String) parameters.get(INVOICE_STATUS))){
+            invoiceStatus = InvoiceStatus.fromDisplayName((String) parameters.get(INVOICE_STATUS));
+        }
+        return invoiceStatus != null ? invoiceStatus : InvoiceStatus.NO_STATUS;
     }
 }
