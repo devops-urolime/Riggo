b6b4f36798fb5c2210de9f6b8c7a8e877d0b2311
edwardsong <pyNN7KTnwVqtbH>
2019-10-14 17:14:05 -0400

    LoadStop default fix arrival status

--- spring-api-services/src/main/java/io/riggo/web/integration/SalesforceRevenovaConstants.java
+++ spring-api-services/src/main/java/io/riggo/web/integration/SalesforceRevenovaConstants.java
@@ -4,8 +4,8 @@ public interface SalesforceRevenovaConstants {
 
     String[] POST_PUT_LOAD_IGNORE_PROPERTIES = new String[]{"id", "extSysTenantId", "locationBasedSvcsReq", "createdAt", "updatedAt", "deleted", "referenceNumber", "billOfLadingNo", "carrierQuoteTotal", "carrierInvoiceTotal", "customerQuoteTotal", "customerQuoteTotal", "customerTransportTotal"};
     String[] PUT_LOAD_LINE_ITEM_IGNORE_PROPERTIES = new String[]{"extSysTenantId", "description", "hazMat", "deliveryStopExtSysId", "pickupStopExtSysId", "loadExtSysId", "weight", "name"};
-    String[] POST_LOAD_LOAD_STOP_IGNORE_PROPERTIES = new String[]{"extSysId", "extSysTenantId", "name", "type", "stopNumber", "shippingReceivingHours", "expectedDateTime", "departureDateTime", "appointmentRequired", "appointmentTime", "stopStatus", "carrierStatus"};
-    String[] PUT_LOAD_STOP_IGNORE_PROPERTIES = new String[]{"extSysId", "extSysTenantId", "name", "type", "stopNumber", "shippingReceivingHours", "expectedDateTime", "appointmentRequired", "appointmentTime", "arrivalDate", "departureDateTime", "stopStatus", "carrierStatus"};
+    String[] POST_LOAD_LOAD_STOP_IGNORE_PROPERTIES = new String[]{"extSysId", "extSysTenantId", "name", "type", "stopNumber", "shippingReceivingHours", "expectedDateTime", "departureDateTime", "appointmentRequired", "appointmentTime", "stopStatus", "arrivalStatus", "carrierStatus"};
+    String[] PUT_LOAD_STOP_IGNORE_PROPERTIES = new String[]{"extSysId", "extSysTenantId", "name", "type", "stopNumber", "shippingReceivingHours", "expectedDateTime", "appointmentRequired", "appointmentTime", "arrivalDate", "departureDateTime", "stopStatus", "arrivalStatus", "carrierStatus"};
     String[] POST_PUT_SHIPPER_IGNORE_PROPERTIES = new String[]{"extSysTenantId", "name"};
     String[] POST_PUT_LOAD_STOP_LOCATION_IGNORE_PROPERTIES = new String[]{"extSysTenantId", "name"};
     String[] POST_PUT_LOAD_STOP_LOCATION_ADDRESS_IGNORE_PROPERTIES = new String[]{"extSysTenantId", "name"};
