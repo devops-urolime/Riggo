84a312eff2c988ce02ee05486bc35748c980cab5
edwardsong <pyNN7KTnwVqtbH>
2019-10-28 14:06:25 -0400

    RIG-688 - make available sf_account_id and sf_contact_id in JWT token

--- auth0/rules/addSiteIdToJWT.js
+++ auth0/rules/addSiteIdToJWT.js
@@ -5,6 +5,15 @@ function (user, context, callback) {
     context.idToken[namespace + 'email'] = user.email;
     context.idToken[namespace + 'email_verified'] = user.email_verified;
 
+    user.user_metadata = user.user_metadata || {};
+    if(user.user_metadata.sf_account_id){
+        context.idToken[namespace + 'sf_account_id'] = user.user_metadata.sf_account_id;
+        context.accessToken[namespace + 'sf_account_id'] = user.user_metadata.sf_account_id;
+    }
+    if(user.user_metadata.sf_contact_id){
+        context.idToken[namespace + 'sf_contact_id'] = user.user_metadata.sf_contact_id;           context.accessToken[namespace + 'sf_contact_id'] = user.user_metadata.sf_contact_id;
+    }
+
     context.accessToken[namespace + 'siteId'] = '100';
     context.accessToken[namespace + 'email'] = user.email;
     context.accessToken[namespace + 'email_verified'] = user.email_verified;
